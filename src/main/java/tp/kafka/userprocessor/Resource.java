ebfae45ebfcc877c56e22a62fbf7455d4fbc5f98
tobias.waller@googlemail.com <tobias.waller@googlemail.com>
2020-10-07 23:36:11 +0000

    api ktable

---
+++ src/main/java/tp/kafka/userprocessor/Resource.java
@@ -0,0 +1,41 @@
+package tp.kafka.userprocessor;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+
+import lombok.extern.java.Log;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Log
+public class Resource {
+    @Inject
+    KafkaStreams streams;
+
+    @Inject
+    Topology topology;
+
+    @GET
+    public ArrayList<User> readLocalData(){
+        var store = streams.store(
+                 StoreQueryParameters.fromNameAndType("usersById", QueryableStoreTypes.<String, User>keyValueStore())
+        );
+        
+        var result = new ArrayList<User>();
+        store.all().forEachRemaining(kv -> result.add(kv.value));
+        return result;
+    }
+
+}
